441a195ac40890d860dc7592d614fdc106831dac
danomatika <danomatika@gmail.com>
2022-10-28 11:08:25 +0200

    remove trailing whitespace, consisten bracket formatting

--- helloworld.c
+++ helloworld.c
@@ -1,26 +1,24 @@
-#include "m_pd.h"  
- 
-static t_class *helloworld_class;  
- 
-typedef struct _helloworld {  
+#include "m_pd.h"
+
+static t_class *helloworld_class = NULL;
+
+typedef struct _helloworld {
   t_object x_obj;
 } t_helloworld;  
- 
-void helloworld_bang(t_helloworld *x)  
-{
+
+void helloworld_bang(t_helloworld *x) {
   (void)x; // silence unused variable warning
   post("Hello world!");
 }  
- 
-void *helloworld_new(void)  
-{  
-  t_helloworld *x = (t_helloworld *)pd_new(helloworld_class);  
-  return (void *)x;  
-}  
- 
-void helloworld_setup(void) {  
-  helloworld_class = class_new(gensym("helloworld"),  
+
+void *helloworld_new(void) {
+  t_helloworld *x = (t_helloworld *)pd_new(helloworld_class);
+  return (void *)x;
+}
+
+void helloworld_setup(void) {
+  helloworld_class = class_new(gensym("helloworld"),
     (t_newmethod)helloworld_new, NULL,
-    sizeof(t_helloworld), CLASS_DEFAULT, 0);  
-  class_addbang(helloworld_class, helloworld_bang);  
+    sizeof(t_helloworld), CLASS_DEFAULT, 0);
+  class_addbang(helloworld_class, helloworld_bang);
 }
